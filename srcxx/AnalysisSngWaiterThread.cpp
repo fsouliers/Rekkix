0a4894a34ef197841e7c68eed0d65372d531fdf8
François Souliers <francois.souliers@laposte.net>
2016-09-05 10:27:03 +0200

    Corrected multi-threading issue in waiter thread

--- srcxx/AnalysisSngWaiterThread.cpp
+++ srcxx/AnalysisSngWaiterThread.cpp
@@ -5,6 +5,8 @@
  * \author f.souliers
  */
 
+#include <QDebug>
+
 #include "AnalysisSngWaiterThread.h"
 #include "ModelSngReqMatrix.h"
 
@@ -24,18 +26,25 @@ void AnalysisSngWaiterThread::run()
 	// 2nd Step of analysis : wait until all threads have terminated their work
 	bool allFinished ;
 	QVector<RequirementFileAbstractPtr>::Iterator it_rf ;
-	do
+	QVector<RequirementFileAbstractPtr> terminatedFiles ;
+
+	while (!allFinished)
 	{
 		allFinished = true ; // by default, suppose all threads are terminated
 
+		// For each file, test if the parsing is terminated
 		for (it_rf = __registeredFiles.begin() ; it_rf != __registeredFiles.end() ; ++it_rf)
 		{
+			// if the file has been parsed, no need to waste time
+			if (terminatedFiles.contains((*it_rf))) continue ;
+
 			// Only wait for threads that have been actually launched (otherwise waits return true in any case),
 			// and only wait for 5ms for termination
 			if ((*it_rf)->isThreadActuallyLaunched() && (*it_rf)->wait(5))
 			{
+				qDebug() << "AnalysisSngWaiterThread::run : fin d'analyse de " << (*it_rf)->getFileId() ;
 				ModelSngReqMatrix::instance().updateRequirementFileWithAcceptedRequirements(*it_rf) ;
-				__registeredFiles.remove(__registeredFiles.indexOf(*it_rf)) ;
+				terminatedFiles.append((*it_rf)) ;
 			}
 			else
 			{
@@ -45,8 +54,12 @@ void AnalysisSngWaiterThread::run()
 		}
 
 		// Don't overload the CPU, if the threads hasn't been launched yet, we can wait a little bit
-		QThread::msleep(50) ;
-	} while (!allFinished) ;
+		if (!allFinished)
+		{
+			qDebug() << "AnalysisSngWaiterThread::run : attente ..." ;
+			QThread::msleep(50) ;
+		}
+	}
 
 	// now all the files have been parsed, let's emit the information
 	emit allRegisteredTerminated() ;
